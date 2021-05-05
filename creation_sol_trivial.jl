2b86c3429ed7e5acf0d29d71e3949ae8f523c4b6
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2021-05-05 19:28:30 +0200

    taff

--- creation_sol_trivial.jl
+++ creation_sol_trivial.jl
@@ -3,6 +3,22 @@ include("parserIn.jl")
 
 inst = read_instance("Instance")
 
-function creation_sol(inst, nb_grpe, dist)
-    
+function creation_sol(inst, nb_grpe, nb_soustraite)
+    """Fonction qui crée des solutions en se basant sur plusieurs paramètres optimisable"""
+    fournisseurs = inst.f
+    if (length(fournisseurs) - nb_soustraite) / nb_grpe > 4
+        return 1e10
+    end
+    sous_traite = []
+    for i in 1:nb_soustraité
+        push!(sous_traite, fournisseurs[i].idx)
+    end
+    Groupe = []
+    nb_f = floor((length(fournisseurs) - nb_soustraite) / nb_grpe)
+    for g in 1:min(nb_grpe, length(fournisseurs))
+        push!(Groupe, [fournisseurs[i + nb_soustraite].idx for i in ((g-1)*nb_f + 1):max(g*nb_f, length(fournisseurs))])
+    end
+    Tournees = []
+
+    return Solution(Sous_traite = sous_traite, Groupe = Groupe, Tournees = Tournees)
 end
