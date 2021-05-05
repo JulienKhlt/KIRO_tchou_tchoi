1bb33050be582e01101c24e0f7745b7c82867f0f
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2021-05-05 18:55:43 +0200

    First step opti

--- opti.jl
+++ opti.jl
@@ -6,12 +6,23 @@ using Optim
 
 f(x) = x[1]^2 + x[2]^2
 
-function g!(G, x)
-    step = 0.1
-    G[1] = (f(x + [1, 0]*step) - f(x)) / step
-    G[2] = (f(x + [0, 1]*step) - f(x)) / step
+function dg(n, i)
+    dx = zeros(n)
+    dx[i] = 1.0
+    return dx
+end
+
+function grad(G, x, step)
+    for i in 1:length(G)
+        G[i] = (f(x + dg(length(G), i)*step[i]) - f(x)) / step[i]
     end
+end
 
-
+function get_best_sol(f, x0, step)
+    g!(G, x) = grad(G, x, step)
+    opt = optimize(f, g!, x0)
+    return Optim.minimizer(opt), Optim.minimum(opt)
+end
 x0 = [0.0, 1.5]
-println(optimize(f, g!, x0))
+step = [0.01, 0.1]
+println(get_best_sol(f, x0, step))
