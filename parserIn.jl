68d2f32440cda55ebfd597b21e2dade2f12ea139
aubin-tchoi <aubin.tchoi@gmail.com>
2021-05-05 14:00:28 +0200

    updated parser

--- parserIn.jl
+++ parserIn.jl
@@ -13,8 +13,7 @@ function read_vertice(row::String)::NamedTuple
     row_split = split(row, r"\s+")
     return (
         idx = parse(Int, row_split[2]),
-        gpsx = parse(Int, row_split[4]),
-        gpsy = parse(Int, row_split[5]),
+        gps = coord(parse(Float64, row_split[4]), parse(Float64, row_split[5]))
     )
 end
 
@@ -24,10 +23,14 @@ function read_instance(path::String)::Instance
     end
 
     params = read_parameters(data[1])
+    depot = read_vertice(data[2])
+    usine = read_vertice(data[3])
 
     return Instance(
         Q = params.Q,
         F = params.F,
         H = params.H,
+        depot = depot(depot.idx, depot.gps)
+        usine = usine(usine.idx, usine.gps)
     )
 end
