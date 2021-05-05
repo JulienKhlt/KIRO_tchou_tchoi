a5ae8c985db13228a44c8770bc560ab743f8b4a5
aubin-tchoi <aubin.tchoi@gmail.com>
2021-05-05 17:21:43 +0200

    instance's struct are not mutable anymore'

--- Instance.jl
+++ Instance.jl
@@ -1,27 +1,27 @@
 # Fichier regroupant les structures permettant de construire l'instance
 
-mutable struct Coord
+struct Coord
     x::Float64
     y::Float64
 
     Coord(; x, y) = new(x, y)
 end
 
-mutable struct Depot
+struct Depot
     idx::Int # Indice du sommet correspondant
     gps::coord # Coordonnées GPS
 
     Depot(; d, g) = new(d, g)
 end
 
-mutable struct Usine
+struct Usine
     idx::Int # Indice du sommet correspondant
     gps::coord # Coordonnées GPS
 
     Usine(; u, g) = new(u, g)
 end
 
-mutable struct Fournisseur
+struct Fournisseur
     idx::Int # Indice du sommet correspondant
     st_cost::Int # Coût de sous-traitance
     q::Vector{Int} # Volumes par semaines
@@ -30,7 +30,7 @@ mutable struct Fournisseur
     Fournisseur(; idx, st_cost, q, gps) = new(; idx, st_cost, q, gps)
 end
 
-mutable struct Instance
+struct Instance
     Q::Int # Taille des camions
     F::Int # Nombre de fournisseurs
     H::Int # Nombre de semaines
