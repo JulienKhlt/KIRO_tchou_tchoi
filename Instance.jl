a6188c2dfde95027a0755abd5a5590cea5712f9b
aubin-tchoi <aubin.tchoi@gmail.com>
2021-05-05 18:27:42 +0200

    upperCase on struct names

--- Instance.jl
+++ Instance.jl
@@ -9,14 +9,14 @@ end
 
 struct Depot
     idx::Int # Indice du sommet correspondant
-    gps::coord # Coordonnées GPS
+    gps::Coord # Coordonnées GPS
 
     Depot(; d, g) = new(d, g)
 end
 
 struct Usine
     idx::Int # Indice du sommet correspondant
-    gps::coord # Coordonnées GPS
+    gps::Coord # Coordonnées GPS
 
     Usine(; u, g) = new(u, g)
 end
@@ -25,18 +25,18 @@ struct Fournisseur
     idx::Int # Indice du sommet correspondant
     st_cost::Int # Coût de sous-traitance
     q::Vector{Int} # Volumes par semaines
-    gps::coord # Coordonnées GPS
+    gps::Coord # Coordonnées GPS
 
-    Fournisseur(; idx, st_cost, q, gps) = new(; idx, st_cost, q, gps)
+    Fournisseur(; idx, st_cost, q, gps) = new(idx, st_cost, q, gps)
 end
 
 struct Instance
     Q::Int # Taille des camions
     F::Int # Nombre de fournisseurs
     H::Int # Nombre de semaines
-    d::depot # Dépôt
-    u::usine # Usine
-    f::Vector{fournisseur} # fournisseurs
+    d::Depot # Dépôt
+    u::Usine # Usine
+    f::Vector{Fournisseur} # fournisseurs
     dist::Matrix{Int} # Graphe des coûts
 
     Instance(; Q, F, H, d, u) = new(Q, F, H, d, u)
