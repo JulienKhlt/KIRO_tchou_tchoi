97b3b86aaaa963c55d854fafa660bcb2fb8eef43
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2021-05-06 12:56:55 +0200

    aaaaahahaha

--- julien.jl
+++ julien.jl
@@ -1,6 +1,6 @@
 # include("creation_sol_trivial.jl")
-# include("Instance.jl")
-# include("parserIn.jl")
+include("Instance.jl")
+include("parser_in.jl")
 include("Solution.jl")
 include("parser_out.jl")
 # include("couts.jl")
@@ -22,11 +22,15 @@ include("parser_out.jl")
 # eval_sol(x) = couts(creation_sol(instance, trunc(Int, x[1]), trunc(Int, x[2])), instance)
 # println(@time get_best_sol(eval_sol, [100., 100.], [0.5, 1.]))
 # println(@time get_best_sol(eval_sol, [100., 100.], [0.5, 1.]))
+inst = read_instance("instances/A.json")
 Non_Affecte = []
-for i in 0:442
-    push!(Non_Affecte, Affectation_Train(id_train=i, voie_Quai="notAffected", it="notAffected"))
+for i in 1:length(inst.trains)
+    for j in 1:length(inst.trains[i])
+        push!(Non_Affecte, Affectation_Train(id_train=inst.trains[i][j].id, voie_Quai="notAffected", it="notAffected"))
+    end
 end
 # Affecte = [Affectation_Train(id_train=0, voie_Quai="", it="")]
 Affecte = []
 sol = Solution(Non_Affecte=Non_Affecte, Affecte=Affecte)
+#println(sol)
 parser_out(sol, "test.json")
