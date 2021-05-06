d1bbc8529149cbe7378d8860f8c156675f30f817
clemgris <clemence.grislain@eleves.enpc.fr>
2021-05-06 13:53:57 +0200

    hello

--- couts.jl
+++ couts.jl
@@ -13,9 +13,9 @@ function couts(sol::Solution, inst::Instance, verbose::Bool = false)
     for t in sol.Affecte
         for tp in sol.Affecte
             t1 = t.id
-            i1 = t.it
+            i1 = t.it.id
             t2 = tp.id
-            i2 = tp.it
+            i2 = tp.it.id
             for cont in inst.contraintes
                 if t1 == cont[1] && i1 == cont[2] && t2 == cont[3] && i2 == cont[4]
                     c_n +=cont[5]
@@ -30,4 +30,16 @@ function couts(sol::Solution, inst::Instance, verbose::Bool = false)
     end
 
     return c_n_a + c_n
+end
+
+
+function couts_train(sol::Solution, inst::Instance, train::Affectation_Train, verbose::Bool = false)
+    if train.id =="Non_Affecte"
+        return 0
+    end
+    c = 0
+    for i in range length(sol.Affecte)
+        t1=train.id
+
+    end
 end
